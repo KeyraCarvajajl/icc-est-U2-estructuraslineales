7e68e4101c663382fee7042374383a100790c833
Keyra Carvajal <keyracarvajalj26@gmail.com>
2025-06-03 08:58:59 -0500

    Estrcuturalineal

---
+++ src/models/Node.java
@@ -0,0 +1,33 @@
+package models;
+
+public class Node {
+    
+    private int value; // Valor del Nodo
+    private Node next; // Referencia del Nodo 
+
+    public Node(int value) {
+        this.value = value;
+        this.next = null;
+    }
+
+    @Override
+    public String toString() {
+        return "Node [value=" + value + ", next=" + next + "]";
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+}
